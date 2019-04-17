34a4540d223e6afc1c13e8dc71c89371fcdec707
Miguel Yann <tsambamiguelito@gmail.com>
2019-04-17 21:27:33 +0200

    (Routing)Add all routes for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DinosauresComponent } from './dinosaures/dinosaures.component';
+import { DinosauresDetailsComponent } from './dinosaures-details/dinosaures-details.component';
+import { DinosauresCreateComponent } from './dinosaures-create/dinosaures-create.component';
+import { DinosauresEditComponent } from './dinosaures-edit/dinosaures-edit.component';
 
-const routes: Routes = [];
-
+const appRoutes: Routes = [
+  {
+    path: 'dinosaures',
+    component: DinosauresComponent,
+    data: { title: 'Dinosaures List' }
+  },
+  {
+    path: 'dinosaures-details/:id',
+    component: DinosauresDetailsComponent,
+    data: { title: 'Dinosaures Details' }
+  },
+  {
+    path: 'dinosaures-create',
+    component: DinosauresCreateComponent,
+    data: { title: 'Create Dinosaure' }
+  },
+  {
+    path: 'dinosaures-edit/:id',
+    component: DinosauresEditComponent,
+    data: { title: 'Edit Book' }
+  },
+  { path: '',
+    redirectTo: '/dinosaures',
+    pathMatch: 'full'
+  }]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
