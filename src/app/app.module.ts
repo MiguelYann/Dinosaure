51c8f2e77404bdbde204b0f406a46f45ccdd266c
Miguel Yann <tsambamiguelito@gmail.com>
2019-04-17 21:54:42 +0200

    (Style) Install and add module for Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DinosauresComponent } from './dinosaures/dinosaures.component';
+import { DinosauresDetailsComponent } from './dinosaures-details/dinosaures-details.component';
+import { DinosauresCreateComponent } from './dinosaures-create/dinosaures-create.component';
+import { DinosauresEditComponent } from './dinosaures-edit/dinosaures-edit.component';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DinosauresComponent,
+    DinosauresDetailsComponent,
+    DinosauresCreateComponent,
+    DinosauresEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
